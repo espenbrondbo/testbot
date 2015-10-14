52db0996d4345faf49953cd8bfebde0546b2840b
Espen B <espen.brondbo@gmail.com>
2015-10-14 17:42:23 +0200

    made a simple example config file

---
+++ example_config.py
@@ -0,0 +1,11 @@
+bot = dict(
+    username    = 'testbot',
+    server      = 'irc.freenode.net',
+    port        = 6667,
+    channel     = '#example'
+)
+
+weather = dict(
+    apikey      = 'your_api_key_for_openweathermap_here',
+    base_url    = 'http://api.openweathermap.org/data/2.5/weather?q='
+)
