812d5c5339f0fc43b2b7fe474fe038e6495cffa0
Espen B <espen.brondbo@gmail.com>
2015-10-14 17:35:19 +0200

    added comments to methods

--- bot_functions.py
+++ bot_functions.py
@@ -17,12 +17,22 @@ def is_weather_command(message):
     return message.startswith('!weather')
 
 def get_city_name_from_message(message):
+    """Extract the city name from a received weather request
+
+    Keyword argument:
+    message -- message reveiced from a channel or from a user
+    """
     info = message.split(' ')
-    if len(info) > 1:
+    if len(info) > 1:   # must be greater than 1 to include a city name
         city = info[1]
         return weather.get_weather_string(city)
     else:
         return 'Could not find weather data for {}'.format(city)
 
 def get_user_name_from_message(message):
+    """Extract user name from a received message
+
+    Keyword argument:
+    message -- message reveiced from user or channel
+    """
     return message.split('!')[0]
